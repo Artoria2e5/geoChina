2bfeafbdd88379390f45c5760f6fa309e8d30c47
caijun <tony.tsai.2046@gmail.com>
2016-03-11 13:55:08 +0800

    version 1.2.7

--- R/geocode.R
+++ R/geocode.R
@@ -6,15 +6,15 @@
 #' of Use at \url{http://developer.baidu.com/map/law.htm}.
 #' 
 #' @param address a character vector specifying a location of interest (e.g., 
-#' "Tsinghua Univeristy")
-#' @param api use google or baidu maps api
+#' "Tsinghua Univeristy").
+#' @param api use Google or Baidu Maps API. When using Baidu Maps API, the address must be in Chinese.
 #' @param key an api key must be provided when calling baidu maps api. 
 #' While it's unnecessary for calling google maps api.
 #' @param ocs output coordinate systems including WGS-84, GCJ-02 and BD-09, which
 #' are the GCSs of Google Earth, Google Map in China and Baidu Map, respectively. 
 #' For address out of China, ocs is automatically set to 'WGS-84' and other values 
 #' are igored.
-#' @param output lat/lng coordinates or lat/lng coordinates with confidence
+#' @param output lat/lng coordinates or lat/lng coordinates with location type (Goolge Map) | confidence #' (Baidu Map) or lat/lng coordinates with formated address and address components (only available for #' Google Map API).
 #' @param messaging turn messaging on/off. The default value is FALSE.
 #' @param time the time interval to geocode, in seconds. Default value is zero. 
 #' When you geocode multiple addresses, set a proper time interval to avoid 
@@ -37,27 +37,31 @@
 #'         messaging = TRUE)
 #' geocode('Beijing railway station', api = 'google', ocs = 'WGS-84', 
 #'         output = 'latlngc')
+#' geocode('Beijing railway station', api = 'google', ocs = 'WGS-84', 
+#'         output = 'latlnga')
 #' geocode(c('Tsinghua University', 'Beijing railway station'), api = 'google', 
 #'         ocs = 'GCJ-02')
 #' geocode(c('Tsinghua University', 'Beijing railway station'), api = 'google', 
 #'         ocs = 'WGS-84', output = 'latlngc', messaging = TRUE)
 #' geocode(c('Tsinghua University', 'Beijing railway station'), api = 'google', 
+#'         ocs = 'WGS-84', output = 'latlnga', messaging = TRUE)
+#' geocode(c('Tsinghua University', 'Beijing railway station'), api = 'google', 
 #'         ocs = 'WGS-84', output = 'latlngc', messaging = TRUE, time = 2)
 #' geocode('Beijing railway station', api = 'baidu', key = 'your baidu maps api key', 
-#'         ocs = 'BD-09')
+#' ocs = 'BD-09')
 #' geocode('Beijing railway station', api = 'baidu', key = 'your baidu maps api key', 
-#'         ocs = 'GCJ-02', messaging = TRUE)
+#' ocs = 'GCJ-02', messaging = TRUE)
 #' geocode('Beijing railway station', api = 'baidu', key = 'your baidu maps api key', 
-#'         ocs = 'BD-09', output = 'latlngc')
+#' ocs = 'BD-09', output = 'latlngc')
 #' geocode(c('Tsinghua University', 'Beijing railway station'), api = 'baidu', 
-#'         key = 'your baidu maps api key', ocs = 'BD-09')
+#' key = 'your baidu maps api key', ocs = 'BD-09')
 #' geocode(c('Tsinghua University', 'Beijing railway station'), api = 'baidu', 
-#'         key = 'your baidu maps api key', ocs = 'WGS-84', output = 'latlngc')
+#' key = 'your baidu maps api key', ocs = 'WGS-84', output = 'latlngc')
 #' }
 
 geocode <- function(address, api = c('google', 'baidu'), key = '', 
                     ocs = c('WGS-84', 'GCJ-02', 'BD-09'), 
-                    output = c('latlng', 'latlngc'), messaging = FALSE, 
+                    output = c('latlng', 'latlngc', 'latlnga'), messaging = FALSE, 
                     time = 0){  
   # check parameters
   stopifnot(is.character(address))
@@ -69,28 +73,26 @@ geocode <- function(address, api = c('google', 'baidu'), key = '',
   stopifnot(is.numeric(time))
   
   # vectorize for many addresses
-  if(length(address) > 1){
-    if(api == 'google'){
+  if (length(address) > 1) {
+    if (api == 'google') {
       s <- 'google restricts requests to 2500 requests a day.'
-      if(length(address) > 2500) stop(s, call. = F)
-      if(length(address) > 200 & messaging) message(paste('Reminder', s, sep = ' : '))
+      if (length(address) > 2500) stop(s, call. = F)
+      if (length(address) > 200 & messaging) message(paste('Reminder', s, sep = ' : '))
     }
-          
-    if(output == 'latlng' | output == 'latlngc'){
-      return(ldply(address, function(add){
-        Sys.sleep(time)
-        geocode(add, api = api, key = key, ocs = ocs, output = output, messaging = messaging)
-      }))
-    }
+    
+    return(ldply(address, function(add){
+      Sys.sleep(time)
+      geocode(add, api = api, key = key, ocs = ocs, output = output, messaging = messaging)
+    }))
   }
   
   # location encoding
   address <- enc2utf8(address)
   # different google maps api is used based user's location. If user is inside China,
   # ditu.google.cn is used; otherwise maps.google.com is used.
-  if(api == 'google'){
+  if (api == 'google') {
     cname <- ip.country()
-    if(cname == "CN"){
+    if (cname == "CN") {
       api_url <- 'http://ditu.google.cn/maps/api/geocode/json'
     } else{
       api_url <- 'http://maps.googleapis.com/maps/api/geocode/json'
@@ -102,40 +104,40 @@ geocode <- function(address, api = c('google', 'baidu'), key = '',
   # https is only supported on Windows, when R is started with the --internet2 
   # command line option. without this option, or on Mac, you will get the error 
   # "unsupported URL scheme".
-  if(api == 'google'){
+  if (api == 'google') {
     # http://maps.googleapis.com/maps/api/geocode/json?address=ADDRESS&sensor
     # =false&key=API_KEY for outside China
     # http://ditu.google.cn/maps/api/geocode/json?address=ADDRESS&sensor
     # =false&key=API_KEY for outside China
     url_string <- paste(api_url, '?address=', address, '&sensor=false', sep = '')
-    if(nchar(key) > 0){
+    if (nchar(key) > 0) {
       url_string <- paste(url_string, '&key=', key, sep = '')
     }
   }
-  if(api == 'baidu'){
+  if (api == 'baidu') {
     # http://api.map.baidu.com/geocoder/v2/?address=ADDRESS&output=json&ak=API_KEY
     url_string <- paste(api_url, '?address=', address, '&output=json&ak=', key, sep = '')
   }
   
-  if(messaging) message(paste('calling ', url_string, ' ... ', sep = ''), appendLF = F)
+  if (messaging) message(paste('calling ', url_string, ' ... ', sep = ''), appendLF = F)
   
   # geocode
   con <- curl(URLencode(url_string))
   gc <- fromJSON(paste(readLines(con, warn = FALSE), collapse = ''))
-  if(messaging) message('done.')  
+  if (messaging) message('done.')  
   close(con)
   
   # geocoding results
-  if(api == 'google'){
+  if (api == 'google') {
     # did geocode fail?
-    if(gc$status != 'OK'){
+    if (gc$status != 'OK') {
       warning(paste('geocode failed with status ', gc$status, ', location = "', 
                     address, '"', sep = ''), call. = FALSE)
       return(data.frame(lat = NA, lng = NA))  
     }
     
     # more than one location found?
-    if(length(gc$results) > 1 && messaging){
+    if (length(gc$results) > 1 && messaging) {
       Encoding(gc$results[[1]]$formatted_address) <- "UTF-8"
       message(paste('more than one location found for "', address, 
                     '", using address\n"', tolower(gc$results[[1]]$formatted_address), 
@@ -146,24 +148,59 @@ geocode <- function(address, api = c('google', 'baidu'), key = '',
       data.frame(lat = NULLtoNA(geometry$location['lat']), 
                  lng = NULLtoNA(geometry$location['lng']), 
                  loctype = tolower(NULLtoNA(geometry$location_type)), 
+                 address = tolower(NULLtoNA(formatted_address)),
                  row.names = NULL)})
     
+    # address components
+    attrdf <- ldply(gc$results[[1]]$address_components, function(l){
+      l <- lapply(l, function(li) {
+        if (length(li) == 0) li <- NA else li
+      })
+      as.data.frame(l, stringsAsFactors = FALSE)[1, ]
+    })
+    attrdf <- attrdf[!is.na(attrdf$types), c('types', 'long_name')]
+    gcdf <- within(gcdf, {
+      poi <- 
+        tolower(NULLtoNA(attrdf$long_name[attrdf$types == 'point_of_interest']))
+      street_no <- 
+        as.numeric(NULLtoNA(attrdf$long_name[attrdf$types == 'street_number']))    
+      route <- 
+        tolower(NULLtoNA(attrdf$long_name[attrdf$types == 'route']))
+      subloc_l3 <- 
+        tolower(NULLtoNA(attrdf$long_name[attrdf$types == 'sublocality_level_3'])) # 村
+      subloc_l2 <- 
+        tolower(NULLtoNA(attrdf$long_name[attrdf$types == 'sublocality_level_2'])) # 镇
+      subloc_l1 <- 
+        tolower(NULLtoNA(attrdf$long_name[attrdf$types == 'sublocality_level_1'])) # 区|县
+      locality <- 
+        tolower(NULLtoNA(attrdf$long_name[attrdf$types == 'locality'])) # 地级市
+      admin_area_l2 <- 
+        tolower(NULLtoNA(attrdf$long_name[attrdf$types == 'administrative_area_level_2'])) # US county
+      admin_area_l1 <- 
+        tolower(NULLtoNA(attrdf$long_name[attrdf$types == 'administrative_area_level_1'])) # 省|直辖市 or US state
+      country <- 
+        tolower(NULLtoNA(attrdf$long_name[attrdf$types == 'country']))      
+      postal_code <- 
+        tolower(NULLtoNA(attrdf$long_name[attrdf$types == 'postal_code']))          
+    })
+    
     # convert coordinates only in China
-    if(!outofChina(gcdf[, 'lat'], gcdf[, 'lng'])){
+    if (!outofChina(gcdf[, 'lat'], gcdf[, 'lng'])) {
       gcdf[c('lat', 'lng')] <- conv(gcdf[, 'lat'], gcdf[, 'lng'], from = 'GCJ-02', to = ocs)
     } else{
-      if(ocs != 'WGS-84'){
+      if (ocs != 'WGS-84') {
         message('wrong usage: for address out of China, ocs can only be set to "WGS-84"', appendLF = T)
         ocs <- 'WGS-84'
       }
     }
     
-    if(output == 'latlng') return(gcdf[c('lat', 'lng')])
-    if(output == 'latlngc') return(gcdf[c('lat', 'lng', 'loctype')])
+    if (output == 'latlng') return(gcdf[c('lat', 'lng')])
+    if (output == 'latlngc') return(gcdf[c('lat', 'lng', 'loctype')])
+    if (output == 'latlnga') return(gcdf[!names(gcdf) %in% c('loctype')])
   }
-  if(api == 'baidu'){
+  if (api == 'baidu') {
     # did geocode fail?
-    if(gc$status != 0){
+    if (gc$status != 0) {
       warning(paste('geocode failed with status code ', gc$status, ', location = "', 
                     address, '". see more details in the response code table of Baidu Geocoding API', 
                     sep = ''), call. = FALSE)
@@ -178,22 +215,26 @@ geocode <- function(address, api = c('google', 'baidu'), key = '',
     # convert coordinates
     gcdf[c('lat', 'lng')] <- conv(gcdf[, 'lat'], gcdf[, 'lng'], from = 'BD-09', to = ocs)
     
-    if(output == 'latlng') return(gcdf[c('lat', 'lng')])
-    if(output == 'latlngc') return(gcdf[c('lat', 'lng', 'conf')])
+    if (output == 'latlnga') {
+      message('Baidu map geocoder cannot return address, please try Goolge map geocoder.', appendLF = T)
+      output <- 'latlng'
+    }
+    if (output == 'latlng') return(gcdf[c('lat', 'lng')])
+    if (output == 'latlngc') return(gcdf[c('lat', 'lng', 'conf')])
   }
 }
 
 # fill NULL with NA
 NULLtoNA <- function(x){
-  if(is.null(x)) return(NA)
-  if(is.character(x) & nchar(x) == 0) return(NA)
+  if (is.null(x)) return(NA)
+  if (is.character(x) & length(x) == 0) return(NA)
   x
 }
 
 # option ip.country to store IP country and avoid calling geohost() repeatedly 
 # when geocoding multiple addresses or revgeocoding multiple locations
 ip.country <- function(){
-  if(!"ip.country" %in% names(options())) {
+  if (!"ip.country" %in% names(options())) {
     options(ip.country = geohost(api = "ipinfo.io")["country"])
   }
   getOption("ip.country")
--- tests/testthat/test-geocode.R
+++ tests/testthat/test-geocode.R
@@ -2,6 +2,6 @@ context("geocode")
 
 test_that("geocode with google api", {
   coords <- geocode('Tsinghua University', api = 'google')
-  expect_true(abs(coords$lat - 39.9984) < 1e-4)
-  expect_true(abs(coords$lng - 116.3203) < 1e-4)
+  expect_true(abs(coords$lat - 39.9984) < 5e-3)
+  expect_true(abs(coords$lng - 116.3203) < 5e-3)
 })
